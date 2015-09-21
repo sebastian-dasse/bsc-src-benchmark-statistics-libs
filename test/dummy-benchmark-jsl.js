657a9b0deae8f1074cbf445af2d157f26e2128e7
sebastian-dasse <s50602@beuth-hochschule.de>
2015-09-21 23:02:00 +0200

    Benchmark functional vs classic JS.

--- test/dummy-benchmark-jsl.js
+++ test/dummy-benchmark-jsl.js
@@ -15,3 +15,27 @@ JSLitmus.test("Dummy2#sum2", function(count) {
     sum2(100000);
   }
 });
+
+JSLitmus.test("Dummy3#sumArray1", function(count) {
+  while (count--) {
+    sumArray1([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  }
+});
+
+JSLitmus.test("Dummy3#sumArray2", function(count) {
+  while (count--) {
+    sumArray2([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  }
+});
+
+JSLitmus.test("Dummy3#mapArray1", function(count) {
+  while (count--) {
+    mapArray1([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], Math.sqrt);
+  }
+});
+
+JSLitmus.test("Dummy3#mapArray2", function(count) {
+  while (count--) {
+    mapArray2([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], Math.sqrt);
+  }
+});
